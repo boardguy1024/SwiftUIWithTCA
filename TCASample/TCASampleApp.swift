567611762788c005c1f3edfd549fb3ad056b2347
パク <ptn-kyungsuk.park@zozo.com>
2023-04-09 23:35:14 +0900

    Initial Commit

---
+++ TCASample/TCASampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCASampleApp.swift
+//  TCASample
+//
+//  Created by パク on 2023/04/09.
+//
+
+import SwiftUI
+
+@main
+struct TCASampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
